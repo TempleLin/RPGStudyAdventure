efb1efd7f10e7008fda740506bbee28e8d365d93
TempleLo <templelo5408@gmail.com>
2021-12-05 21:37:18 +0800

    Reduce more performance cost.

--- Assets/AttackSelectionUtility.cs
+++ Assets/AttackSelectionUtility.cs
@@ -26,10 +26,10 @@ public class AttackSelectionUtility : PageUtility
     // Update is called once per frame
     void Update()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
         monsterSelectionWidgets.ForEach(selections =>
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
             selections.checkOnHover(hit);
             selections.checkOnClick();
         });
