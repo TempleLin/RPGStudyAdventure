e2ac9076bc8873248e5c876be68d72de0cc5be76
TempleLo <templelo5408@gmail.com>
2021-12-07 23:57:25 +0800

    Add status panel.

--- Assets/MainCharStatusUtility.cs
+++ Assets/MainCharStatusUtility.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class MainCharStatusUtility : PageUtility {
     [SerializeField] private GameObject inventoryObject;
+    [SerializeField] private GameObject statusPanelObject;
     private GameObject mainChar;
     private Vector3 mainCharOriginalPos;
     void Start() {
         base.utilityStart();
         inventoryObject.SetActive(false);
+        statusPanelObject.SetActive(false);
         mainChar = _utilitiesSharedData.MainCharObject;
     }
 
     public override void getCalledStart() {
         inventoryObject.SetActive(true);
+        statusPanelObject.SetActive(true);
         _utilitiesSharedData.MainCharObject.SetActive(true);
         Vector3 tempPosRef = mainChar.transform.position;
         mainCharOriginalPos = tempPosRef;
@@ -25,6 +28,7 @@ public class MainCharStatusUtility : PageUtility {
 
     public override void getCalledStop() {
         inventoryObject.SetActive(false);
+        statusPanelObject.SetActive(false);
         _utilitiesSharedData.MainCharObject.SetActive(false);
         mainChar.transform.position = mainCharOriginalPos;
     }
