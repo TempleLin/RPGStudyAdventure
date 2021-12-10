f6b2547807a5b4746e54666bcc9bf4099dcadd75
TempleLo <templelo5408@gmail.com>
2021-12-10 13:15:39 +0800

    Add main character preview to shop.

--- Assets/ShopUtility.cs
+++ Assets/ShopUtility.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ShopUtility : PageUtility {
-    [SerializeField]
-    private GameObject shopLadyObject;
-    [SerializeField]
-    private GameObject shopPageUIObject;
+    [SerializeField] private GameObject shopLadyObject;
+    [SerializeField] private GameObject shopPageUIObject;
+    [SerializeField] private GameObject mainCharImg;
+    [SerializeField] private GameObject mainCharPreviewPanel;
+    private Vector3 mainCharImgOriginPos;
     void Start() {
         base.utilityStart();
         shopLadyObject.SetActive(false);
         shopPageUIObject.SetActive(false);
+        mainCharImgOriginPos = mainCharImg.transform.position;
     }
 
     public override void getCalledStart() {
         shopLadyObject.SetActive(true);
         shopPageUIObject.SetActive(true);
+        mainCharImg.transform.position = mainCharPreviewPanel.transform.position;
+        mainCharImg.SetActive(true);
     }
 
     public override void getCalledUpdate() {
@@ -24,5 +28,7 @@ public class ShopUtility : PageUtility {
     public override void getCalledStop() {
         shopLadyObject.SetActive(false);
         shopPageUIObject.SetActive(false);
+        mainCharImg.transform.position = mainCharImgOriginPos;
+        mainCharImg.SetActive(false);
     }
 }
