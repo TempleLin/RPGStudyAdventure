ac10172a5b65959dba55ed18ee93999806124797
TempleLo <templelo5408@gmail.com>
2021-12-10 19:03:50 +0800

    Remove deprecated code.

--- Assets/ItemInShopList.cs
+++ Assets/ItemInShopList.cs
@@ -8,13 +8,6 @@ public class ItemInShopList : MonoBehaviour
     private Text text;
     private ItemInfo itemInfo;
     public ItemInfo @ItemInfo { set => itemInfo = value; }
-    void Start()
-    {
-        //text = GetComponent<Text>();
-        //if (text == null) {
-        //    Debug.Log("Failed");
-        //}
-    }
     public void getCalledStart() {
         text = GetComponent<Text>();
         text.text = itemInfo.name;
