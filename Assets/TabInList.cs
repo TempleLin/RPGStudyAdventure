4030d87a8ab91b87e8f7935231edd6f3e5abec12
TempleLo <templelo5408@gmail.com>
2021-12-15 00:28:59 +0800

    Fixed scrollbar not adapting when switching tabs of lists.

--- Assets/TabInList.cs
+++ Assets/TabInList.cs
@@ -10,6 +10,8 @@ public class TabInList : MonoBehaviour
     private List<GameObject> otherLists;
     [SerializeField]
     private GameObject holdingList;
+    [SerializeField]
+    private ScrollRect scrollRect;
 
     void Start()
     {
@@ -17,6 +19,7 @@ public class TabInList : MonoBehaviour
         button.onClick.AddListener(delegate {
             otherLists.ForEach(x => x.SetActive(false));
             holdingList.SetActive(true);
+            scrollRect.content = holdingList.GetComponent<RectTransform>();
         });
     }
 }
